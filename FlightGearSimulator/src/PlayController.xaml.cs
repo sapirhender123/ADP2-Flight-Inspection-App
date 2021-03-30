17cce5a62c86086386ef3c93ea842ed5fd1636d0
Sapir Hender <sapirhender@gmail.com>
2021-03-31 00:05:57 +0300

    src: Add PlayController XAML

---
+++ FlightGearSimulator/src/PlayController.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace FlightGearSimulator.src
+{
+    /// <summary>
+    /// Interaction logic for PlayController.xaml
+    /// </summary>
+    public partial class PlayController : Window
+    {
+        readonly PlayerViewModel vm;
+        public PlayController()
+        {
+            InitializeComponent();
+            vm = new PlayerViewModel(null); // TODO: new PlayerModel(new TCPConnection())
+            DataContext = vm;
+        }
+    }
+}
