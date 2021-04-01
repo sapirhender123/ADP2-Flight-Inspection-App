c2ee5309e0d5274a2f6dff4510c1ee4f29cf9b6b
Sapir Hender <sapirhender@gmail.com>
2021-04-01 18:40:17 +0300

    Add a limit for the slider and format of the time

--- FlightGearSimulator/src/PlayController.xaml.cs
+++ FlightGearSimulator/src/PlayController.xaml.cs
@@ -10,45 +10,66 @@ namespace FlightGearSimulator.src
     public partial class PlayController : Window
     {
         readonly PlayerViewModel vm;
-        private float prev_speed;
+        private float prev_speed = 1;
         public PlayController()
         {
             InitializeComponent();
             vm = new PlayerViewModel(new FlightModel()); // TODO: (new TCPConnection())
+            vm.FG_Speed = 0;
             DataContext = vm;
         }
 
         private void PlayController_Play(object sender, MouseButtonEventArgs e)
         {
-            Console.WriteLine("PlayController_Play");
-            vm.FG_Speed = prev_speed;
+            if (vm.FG_Speed == 0)
+            {
+                vm.FG_Speed = prev_speed;
+            }
         }
 
         private void PlayController_FastFordward(object sender, MouseButtonEventArgs e)
         {
-            Console.WriteLine("PlayController_FastFordward");
-            vm.FG_Speed += 0.1f;
+            if (vm.FG_Speed == 0)
+            {
+                prev_speed += 0.1f;
+            } else
+            {
+                vm.FG_Speed += 0.1f;
+            }
             Console.WriteLine(vm.model.Speed);
         }
 
         private void PlayController_MoveBack(object sender, MouseButtonEventArgs e)
         {
-            Console.WriteLine("PlayController_MoveBack");
-            vm.FG_Speed -= 0.1f;
-            Console.WriteLine(vm.model.Speed);
+            if (vm.FG_Speed == 0)
+            {
+                prev_speed -= 0.1f;
+                if (prev_speed < 0)
+                {
+                    prev_speed = 0;
+                }
+            }
+            else if (vm.FG_Speed > 0) 
+            {
+                vm.FG_Speed -= 0.1f;
+                if (vm.FG_Speed < 0)
+                {
+                    vm.FG_Speed = 0;
+                }
+            }
         }
 
         private void PlayController_Pause(object sender, MouseButtonEventArgs e)
         {
-            Console.WriteLine("PlayController_Pause");
             prev_speed = vm.FG_Speed;
             vm.FG_Speed = 0;
         }
 
         private void PlayController_Stop(object sender, MouseButtonEventArgs e)
         {
-            Console.WriteLine("PlayController_Stop");
-            vm.FG_Time = 0;
+            prev_speed = 1;
+            vm.FG_CurrentTime = 0;
+            vm.FG_Speed = 0;
         }
     }
 }
