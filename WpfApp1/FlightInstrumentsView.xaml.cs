461e12b4b83de0c19d97276cb8f049d4ce931bc5
talco1 <talco9711@gmail.com>
2021-04-07 11:03:22 +0300

    change FlightInstrumentsController to UserControl

--- WpfApp1/FlightInstrumentsView.xaml.cs
+++ WpfApp1/FlightInstrumentsView.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using WpfApp1;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,17 +14,21 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace WpfApp1.User_Controls
 {
     /// <summary>
     /// Interaction logic for FlightInstrumentsView.xaml
     /// </summary>
     public partial class FlightInstrumentsView : UserControl
     {
-
+        private FlightInstrumentsViewModel vm;
         public FlightInstrumentsView()
         {
             InitializeComponent();
+            Model m = new Model();
+            vm = new FlightInstrumentsViewModel(m);
+            DataContext = vm;
+            m.helper();
         }
     }
 }
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -19,14 +19,9 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
-        private FlightInstrumentsViewModel vm;
         public MainWindow()
         {
             InitializeComponent();
-            Model m = new Model();
-            vm = new FlightInstrumentsViewModel(m);
-            DataContext = vm;
-            m.helper();
         }
     }
 }
