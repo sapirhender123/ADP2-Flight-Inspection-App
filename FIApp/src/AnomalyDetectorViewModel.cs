2a24f973bc036a395842c82da28c231e720ab169
Sapir Hender <sapirhender@gmail.com>
2021-04-10 23:51:30 +0300

    src: Move DLL loading logic to separate function in NativeMethods

--- FIApp/src/AnomalyDetectorViewModel.cs
+++ FIApp/src/AnomalyDetectorViewModel.cs
@@ -34,47 +34,14 @@ namespace FIApp
 
         public void LoadPlugin(string pluginPath)
         {
-            if (!File.Exists(pluginPath))
+            if (!File.Exists("reg_flight.csv") || !File.Exists("anomaly_flight.csv"))
             {
+                Console.WriteLine("Missing files");
+                MessageBox.Show("Missing files", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+
                 return;
             }
-
-            Console.WriteLine(String.Format("Loading {0}", pluginPath));
-            try
-            {
-                IntPtr pDll = NativeMethods.LoadLibrary(pluginPath);
-                if (pDll == IntPtr.Zero)
-                {
-                    Console.WriteLine(String.Format("Failed to load DLL {0}", pluginPath));
-                    return;
-                }
-
-                IntPtr pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, "Detect");
-                if (pAddressOfFunctionToCall == IntPtr.Zero)
-                {
-                    Console.WriteLine(String.Format("Could not find method Detect in DLL {0}", pluginPath));
-                    return;
-                }
-
-                Detect detect = (Detect)Marshal.GetDelegateForFunctionPointer(
-                    pAddressOfFunctionToCall, typeof(Detect));
-
-                if (!File.Exists("reg_flight.csv") || !File.Exists("anomaly_flight.csv"))
-                {
-                    Console.WriteLine("Missing files");
-                    MessageBox.Show("Missing files", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
-
-                    return;
-                }
-                detect();
-                bool result = NativeMethods.FreeLibrary(pDll);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Unable to load {0}.", pluginPath);
-                Console.WriteLine(e.Message.Substring(0,
-                                  e.Message.IndexOf(".") + 1));
-            }
+            _ = NativeMethods.CallFuncFromDLLByName(pluginPath, "Detect");
         }
 
         private string DLLPluginPath;
--- FIApp/src/NativeMethods.cs
+++ FIApp/src/NativeMethods.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Runtime.InteropServices;
 
 namespace FIApp
@@ -13,5 +14,45 @@ namespace FIApp
 
         [DllImport("kernel32.dll")]
         public static extern bool FreeLibrary(IntPtr hModule);
+
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        private delegate void Func();
+
+        public static bool CallFuncFromDLLByName(string dllPath, string exportedFuncName)
+        {
+            if (!File.Exists(dllPath))
+            {
+                return false;
+            }
+
+            Console.WriteLine(String.Format("Loading {0}", dllPath));
+            try
+            {
+                IntPtr pDll = NativeMethods.LoadLibrary(dllPath);
+                if (pDll == IntPtr.Zero)
+                {
+                    Console.WriteLine(String.Format("Failed to load DLL {0}", dllPath));
+                    return false;
+                }
+
+                IntPtr pAddressOfFunctionToCall = NativeMethods.GetProcAddress(pDll, exportedFuncName);
+                if (pAddressOfFunctionToCall == IntPtr.Zero)
+                {
+                    Console.WriteLine(String.Format("Could not find method Detect in DLL {0}", dllPath));
+                    return false;
+                }
+
+                Func func = (Func)Marshal.GetDelegateForFunctionPointer(pAddressOfFunctionToCall, typeof(Func));
+                func();
+                return NativeMethods.FreeLibrary(pDll);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to load {0}.", dllPath);
+                Console.WriteLine(e.Message.Substring(0,
+                                  e.Message.IndexOf(".") + 1));
+            }
+            return false;
+        }
     }
 }
