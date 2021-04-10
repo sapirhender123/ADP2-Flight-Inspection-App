e6abc31d2a133ed699cdb057253087bf082e4759
talco1 <talco9711@gmail.com>
2021-04-10 14:17:49 +0300

    added joystick controller

---
+++ WpfApp1/JoystickView.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+using System.Windows.Controls;
+using FIApp.ViewModels;
+
+namespace FIApp.Views
+{
+    /// <summary>
+    /// Interaction logic for JoystickView.xaml
+    /// </summary>
+    public partial class JoystickView : UserControl
+    {
+        private JoystickViewModel vm;
+        public JoystickView()
+        {
+            InitializeComponent();
+            Model m = new Model();
+            vm = new JoystickViewModel(m);
+            DataContext = vm;
+           // m.helper();
+            //m.connect();
+            //m.start();
+        }
+    }
+}
---
+++ WpfApp1/JoystickViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+
+namespace FIApp.ViewModels
+{
+    class JoystickViewModel : INotifyPropertyChanged
+    {
+        private Model model;
+        public JoystickViewModel(Model model)
+        {
+            this.model = model;
+            model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e) 
+            { 
+                NotifyPropertyChanged("VM_Aileron");
+                NotifyPropertyChanged("VM_Elevator");
+                NotifyPropertyChanged("VM_Rudder");
+                NotifyPropertyChanged("VM_Throttle0");
+            };
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (this.PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+
+        //properties
+        public double VM_Aileron
+        {
+            get {
+                return model.Aileron; }
+        }
+        public double VM_Elevator
+        {
+            get { return model.Elevator; }
+        }
+        public double VM_Eudder
+        {
+             get { return model.Rudder; }
+        }
+        public double VM_Throttle0
+        {
+             get { return model.Throttle0; }
+        }
+
+    }
+}
