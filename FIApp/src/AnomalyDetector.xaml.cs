2cdd4a115e62b472455ef2f35aaa4bb651c24514
Sapir Hender <sapirhender@gmail.com>
2021-04-16 17:38:21 +0300

    src: Add AnomalyDetector controller view and viewmodel UI + load DLL

---
+++ FIApp/src/AnomalyDetector.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Windows.Controls;
+
+namespace FIApp.Views
+{
+    /// <summary>
+    /// Interaction logic for AnomalyDetector.xaml
+    /// </summary>
+    public partial class AnomalyDetector : UserControl
+    {
+        readonly AnomalyDetectorViewModel vm;
+        static readonly string pluginsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "plugins");
+        readonly string[] plugins = Directory.GetFiles(pluginsPath, "*.dll");
+
+        public AnomalyDetector()
+        {
+            InitializeComponent();
+
+            vm = new AnomalyDetectorViewModel(model);
+            this.DataContext = vm;
+
+            InitializePluginsSelection();
+        }
+
+        // Update the detection according to the choice
+        private void PluginSelection_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            vm.AD_DLLPluginPath = Path.Combine(pluginsPath, String.Concat(e.AddedItems[0], ".dll"));
+        }
+
+        public void InitializePluginsSelection()
+        {
+            PluginSelection.Items.Insert(0, "Anomaly detection algorithm");
+            PluginSelection.SelectedIndex = 0;
+            foreach (string i in plugins)
+            {
+                // Add each DLL as an option using only its filename (without full path or extention)
+                PluginSelection.Items.Add(Path.GetFileNameWithoutExtension(i));
+            }
+        }
+    }
+}
---
+++ FIApp/src/AnomalyDetectorViewModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+
+namespace FIApp
+{
+    class AnomalyDetectorViewModel : INotifyPropertyChanged
+    {
+        public Model model;
+        public AnomalyDetectorViewModel(Model model)
+        {
+            this.model = model;
+            this.model.PropertyChanged += delegate (Object sender, PropertyChangedEventArgs e)
+            {
+                NotifyPropertyChanged("AD_" + e.PropertyName);
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                // The "propName" was changed, notify the relevant items
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+
+        public void LoadPlugin(string pluginPath)
+        {
+            if (!File.Exists(pluginPath))
+            {
+                return;
+            }
+
+            Console.WriteLine(String.Format("Loading {0}", pluginPath));
+            try
+            {
+                // Load DLL into memory
+                Assembly plugin = Assembly.LoadFrom(pluginPath);
+
+                // Get the type from the assembly
+                Type t = plugin.GetType(String.Format("{0}.{0}", Path.GetFileNameWithoutExtension(pluginPath)));
+
+                // Get the detect method from the DLL (declare the method prototype)
+                MethodInfo DLLdetect = t.GetMethod("Detect", new Type[] { typeof(int[][]) });
+
+                // Create a new instance of the ChordFileGenerator type
+                object o = Activator.CreateInstance(t);
+                // DLLdetect.Invoke(o, model.csvData);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                return;
+            }
+        }
+
+        private string DLLPluginPath;
+        public string AD_DLLPluginPath
+        {
+            get
+            {
+                return DLLPluginPath;
+            }
+            set
+            {
+                DLLPluginPath = value;
+                LoadPlugin(DLLPluginPath);
+            }
+        }
+    }
+}
