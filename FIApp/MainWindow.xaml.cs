286577691c24b6f0780e69c9a0bf960f5185f022
NoamBurgansky <b.noam55@gmail.com>
2021-04-21 23:30:56 +0300

    app design, first version

---
+++ FIApp/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using FIApp;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+static class Globals
+{
+    public static Model model = new Model();
+}
+
+namespace FIApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+
+namespace FIApp.Views
+{
+    public partial class JoystickView : UserControl
+    {
+        Model model = Globals.model;
+    }
+}
+
+namespace FIApp.Views
+{
+    public partial class StartFlightGear : UserControl
+    {
+        Model model = Globals.model;
+    }
+}
+
+namespace FIApp.Views
+{
+    public partial class FlightInstrumentsView : UserControl
+    {
+        Model model = Globals.model;
+    }
+}
+namespace FIApp.Views
+{
+    public partial class PlayController : UserControl
+    {
+        Model model = Globals.model;
+    }
+}
