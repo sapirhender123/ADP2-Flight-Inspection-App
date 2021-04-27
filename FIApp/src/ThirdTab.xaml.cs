4f7c43f4859e4addadaf1bfd9428bcf330c0235f
talco1 <talco9711@gmail.com>
2021-04-27 14:22:36 +0300

    deleted duplicate mainWindow, added ThirdTab, update PlayController and modified gitignore

---
+++ FIApp/src/ThirdTab.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace FIApp.Views
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class SecondTab : UserControl
+    {
+        public SecondTab()
+        {
+            InitializeComponent();
+        }
+    }
+}
